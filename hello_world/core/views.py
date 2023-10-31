15990a5eccdef171094e04e12420d58d733741bf
Kanyaphach <100203584+Kanyaphach@users.noreply.github.com>
2023-10-31 07:38:36 +0000

    add blogpost path

--- hello_world/core/views.py
+++ hello_world/core/views.py
@@ -6,3 +6,6 @@ def index(request):
     }
     return render(request, "ablog/index.html", context)
 
+def blog_post(request):
+    context = {}
+    return render(request, "ablog/blog-post.html", context=context)
--- hello_world/urls.py
+++ hello_world/urls.py
@@ -24,6 +24,8 @@ urlpatterns = [
     path("", core_views.index),
     path("admin/", admin.site.urls),
     path("__reload__/", include("django_browser_reload.urls")),
+    path("blogpost", core_views.blog_post),
+
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
